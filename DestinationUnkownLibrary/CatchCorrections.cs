874f9620f5a417816e7a3dd5b805f6f74524b741
TaylorBrown96 <78773029+TaylorBrown96@users.noreply.github.com>
2022-03-19 05:25:43 -0400

    Reupload of source code

---
+++ DestinationUnkownLibrary/CatchCorrections.cs
@@ -0,0 +1,31 @@
+﻿/**
+* 18MAR22
+* CSC 153
+* Taylor J. Brown
+* This program is a maze/rpg text adventure game. 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DestinationUnkownLibrary
+{
+    // This class will be expanded on to give the user multiple chances to enter in the correct values.
+    public class CatchCorrections
+    {
+        // Prompts the user to enter a username and creates the User_Profile.txt file
+        public static string UserNameCreation()
+        {
+            Console.Write("Please enter in a username: ");
+            string userName = Console.ReadLine();
+            StreamWriter outputFile = File.CreateText("Game_Data/User_Profile.txt");
+            outputFile.WriteLine(userName);
+            outputFile.Close();
+            return userName;
+        }
+    }
+}
---
+++ DestinationUnkownLibrary/Combat.cs
@@ -0,0 +1,28 @@
+﻿/**
+* 18MAR22
+* CSC 153
+* Taylor J. Brown
+* This program is a maze/rpg text adventure game. 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DestinationUnkownLibrary
+{
+    // Will add [Block, Heal, Run] methods to class
+    public class Combat
+    {
+        // Takes in an argument of HP and generates a random number between 1-20 to
+        // subtract from the total and then returns the new HP value.
+        public static int Attack(ref int HP)
+        {
+            Random rnd = new Random();
+            HP -= rnd.Next(1,21);
+            return HP;
+        }
+    }
+}
